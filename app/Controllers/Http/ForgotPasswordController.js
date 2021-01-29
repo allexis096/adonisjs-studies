303b374307c34ec29cde6b8792dcaa8f22178178
allexis <allexissoares@gmail.com>
2021-01-29 10:06:30 -0300

    feat: add update method to functional reset password

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const crypto = require('crypto')
+const moment = require('moment')
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const User = use('App/Models/User')
@@ -40,7 +41,40 @@ class ForgotPasswordController {
             .subject('Recuperação de senha')
         })
     } catch (err) {
-      return response.status(err.status).send({ error: { message: 'Algo não deu certo, esse e-mail existe?' } })
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Algo não deu certo, esse e-mail existe?' } })
+    }
+  }
+
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment().subtract('2', 'days').isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: 'O token de expiração está expirado' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Algo deu errado ao resetar sua senha' } })
     }
   }
 }
