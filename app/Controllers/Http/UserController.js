d225d990956ca39944c7df1929eb106f6ebb3319
allexis <allexissoares@gmail.com>
2021-02-01 09:34:42 -0300

    feat: add relationship between user and useraddress

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -3,11 +3,21 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const User = use('App/Models/User')
 
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+
 class UserController {
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   */
+
   async store ({ request }) {
     const data = request.only(['username', 'email', 'password'])
+    const addresses = request.input('addresses')
 
     const user = await User.create(data)
+
+    await user.addresses().createMany(addresses)
 
     return user
   }
